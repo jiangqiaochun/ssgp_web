f870fafd01ae7f054d5e77be6dd3f931a7972e2d
jiangqiaochun <1225992531@qq.com>
2019-04-24 23:19:19 +0800

    'fix:修复选题接口调用'

--- src/api/student.js
+++ src/api/student.js
@@ -9,3 +9,21 @@ export const getStudentInfo = (userId) => {
     method: 'get'
   })
 }
+
+export const getSelectionData = () => {
+  return axios.request({
+    url: '/projects',
+    method: 'get'
+  })
+}
+
+export const selectProject = ({studentId, projectId}) => {
+  let req = new FormData()
+  req.set('studentId', studentId)
+  req.set('projectId', projectId)
+  return axios.request({
+    url: '/selections/',
+    data: req,
+    method: 'post'
+  })
+}
--- src/view/Student/select/select-manage.vue
+++ src/view/Student/select/select-manage.vue
@@ -6,46 +6,90 @@
       <Button type="primary" style="background-color: #8c1515; border: 0">搜索</Button>
     </div>
     <div class="student-select-table">
-      <Table border :columns="selectColumns" :data="data1" style="margin-top: 10px"></Table>
+      <Table border :columns="selectColumns" :data="selectionData" style="margin-top: 10px"></Table>
     </div>
   </div>
 </template>
 
 <script>
+import {getSelectionData, selectProject} from '@/api/student'
 export default {
   name: 'select-manage',
   data () {
     return {
+      selectionData: [],
       selectColumns: [
         {
           title: '选题名称',
-          key: 'name'
+          key: 'projectName'
         },
         {
           title: '选题性质',
-          key: ''
+          key: 'projectNature'
         },
         {
           title: '选题分类',
-          key: ''
+          key: 'projectType'
         },
         {
           title: '指导老师',
-          key: ''
+          key: 'teacherName'
         },
         {
           title: '已选人数',
           key: 'selectedNum'
         },
         {
-          title: '操作'
+          title: '操作',
+          render: (h, params) => {
+            return h('div', {
+              attrs: {
+                // style: 'height: 40px;'
+              }
+            }, [
+              h('span', {
+                props: {},
+                style: {
+                  cursor: 'pointer',
+                  marginRight: '10px',
+                  color: '#57a3f3'
+                },
+                on: {
+                  click: () => {
+                    this.selectProject(params.row)
+                  }
+                }
+              }, '选择')
+            ])
+          }
         }
       ]
     }
   },
+  mounted () {
+    this.getSelections()
+  },
   methods: {
     goMySelection () {
       this.$router.push({ name: 'mySelection', params: {studentId: localStorage.getItem('userId')} })
+    },
+    getSelections () {
+      getSelectionData().then(res => {
+        if (res.data.code === 200) {
+          this.selectionData = res.data.data
+        }
+      })
+    },
+    selectProject (row) {
+      let req = {
+        studentId: localStorage.getItem('userId'),
+        projectId: row.projectId
+      }
+      selectProject(req).then(res => {
+        if (res.data.code === 200) {
+          this.$Message.success('选题成功！')
+        }
+      })
     }
   }
 }
